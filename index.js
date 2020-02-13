232c043e8f8f6ddcbd54db886a154f3dd78633b6
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2020-02-13 07:10:47 +0100

    Add scrolle feature

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("działa");
   const rootElement = document.querySelector("#root");
   const sections = document.querySelectorAll("section");
-  console.log(sections);
+  let currentSectionIndex = 0;
+
+  let isThrottlet = false;
+
   document.addEventListener("mousewheel", e => {
+    if (isThrottlet) return;
+    isThrottlet = true;
+
+    setTimeout(() => (isThrottlet = false), 1000);
+
     console.log(e.wheelDelta);
+    const direction = e.wheelDelta < 0 ? 1 : -1;
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const isFirstSection = currentSectionIndex === 0;
+      if (isFirstSection) return;
+    }
+    currentSectionIndex += direction;
+    console.log(currentSectionIndex);
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
   });
 });
