5e15145d1c65bf1e3da56e14f21da9d7fa04c514
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2020-02-14 07:44:21 +0100

    Add swiper to detect swipes on touchable devices

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const scroller = new Scroller("#root");
   //   document.addEventListener("mousewheel", scroller.listenScroll);
   document.addEventListener("wheel", scroller.listenScroll);
+  document.addEventListener("swipeUp", () => scroller.scroll(1));
+  document.addEventListener("swipeDown", () => scroller.scroll(-1));
 });
---
+++ swiper.js
@@ -0,0 +1,48 @@
+class Swiper {
+  constructor() {
+    this.initialY = null;
+    this.initialX = null;
+    document.addEventListener("touchstart", this.startTouch);
+    document.addEventListener("touchmove", this.moveTouch);
+    this.events = {
+      swipeUp: new Event("swipeUp"),
+      swipeDown: new Event("swipeDown"),
+      swipeLeft: new Event("swipeLeft"),
+      swipeRight: new Event("swipeRight")
+    };
+  }
+  startTouch = e => {
+    e.preventDefault();
+    console.log(e);
+    this.initialX = e.touches[0].clientX;
+    this.initialY = e.touches[0].clientY;
+    console.log(this.initialY);
+    console.log(this.initialX);
+  };
+  moveTouch = e => {
+    if (!this.initialX || !this.initialY) return;
+    const currentX = e.touches[0].clientX;
+    const currentY = e.touches[0].clientY;
+    const diffX = this.initialX - currentX;
+    const diffY = this.initialY - currentY;
+    console.log(diffX);
+    console.log(diffY);
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+      if (diffX > 0) {
+        document.dispatchEvent(this.events.swipeLeft);
+      } else {
+        document.dispatchEvent(this.events.swipeRight);
+      }
+    } else {
+      if (diffY > 0) {
+        document.dispatchEvent(this.events.swipeUp);
+      } else {
+        document.dispatchEvent(this.events.swipeDown);
+      }
+    }
+    this.initialX = null;
+    this.initialY = null;
+  };
+}
+
+new Swiper();
